c9066f7454cbe1f2ebba4b5200e5f00367b582f3
Michael Rätzel <viir@viir.de>
2024-03-23 08:35:05 +0000

    Logging if certificate was available at the right time
    It looks like the part that will set the certificate in the property consumed here runs asynchronous, so we want to know whether it was available when `KestrelOptionsSetup.Configure` was executed.

--- src/FluffySpoon.AspNet.EncryptWeMust/KestrelOptionsSetup.cs
+++ src/FluffySpoon.AspNet.EncryptWeMust/KestrelOptionsSetup.cs
@@ -14,19 +14,29 @@ namespace FluffySpoon.AspNet.EncryptWeMust
         {
             _logger = logger;
         }
-        
+
         public void Configure(KestrelServerOptions options)
         {
-            if (LetsEncryptRenewalService.Certificate is LetsEncryptX509Certificate x509Certificate)
+            if (LetsEncryptRenewalService.Certificate is not { } certificate)
             {
+                _logger.LogWarning("No certificate found, Kestrel will not be configured to use HTTPS.");
+                return;
+            }
+
+            _logger.LogInformation("Found certificate. Thumbprint: {Thumbprint}", certificate.Thumbprint);
+
+            if (certificate is LetsEncryptX509Certificate x509Certificate)
+            {
+                _logger.LogInformation("Configuring Kestrel to use HTTPS with the certificate.");
+
                 options.ConfigureHttpsDefaults(o =>
                 {
                     o.ServerCertificateSelector = (_a, _b) => x509Certificate.GetCertificate();
                 });
             }
-            else if(LetsEncryptRenewalService.Certificate != null)
+            else
             {
-                _logger.LogError("This certificate cannot be used with Kestrel");
+                _logger.LogError("This certificate type cannot be used with Kestrel: {type}", certificate.GetType().FullName);
             }
         }
     }
